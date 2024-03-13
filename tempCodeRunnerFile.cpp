452fcf92ba154eca23b4543927a7a59eae7bf4e0
ShopnilIsCoding <shopnilkhan766@gmail.com>
2024-03-13 10:03:14 +0600

    kruskals algorithm

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct Edge {
+    int src, dest, weight;
+};
+
+class Graph {
+private:
+    int V;
+    vector<Edge> edges;
+
+public:
+    Graph(int V) : V(V) {}
+
+    void addEdge(int src, int dest, int weight) {
+        Edge edge = {src, dest, weight};
+        edges.push_back(edge);
+    }
+
+    int findParent(vector<int>& parent, int i) {
+        if (parent[i] == -1)
+            return i;
+        return findParent(parent, parent[i]);
+    }
+
+    void unionSets(vector<int>& parent, int x, int y) {
+        int xset = findParent(parent, x);
+        int yset = findParent(parent, y);
+        parent[xset] = yset;
+    }
+
+    void kruskalMST() {
+        vector<int> parent(V, -1);
+        vector<Edge> result;
+        sort(edges.begin(), edges.end(), [](Edge& a, Edge& b) {
+            return a.weight < b.weight;
+        });
+
+        int e = 0, i = 0;
+        while (e < V - 1 && i < edges.size()) {
+            Edge nextEdge = edges[i++];
+            int x = findParent(parent, nextEdge.src);
+            int y = findParent(parent, nextEdge.dest);
+            if (x != y) {
+                result.push_back(nextEdge);
+                unionSets(parent, x, y);
+                e++;
+            }
+        }
+
+        cout << "Edges in MST:" << endl;
+        for (i = 0; i < result.size(); ++i)
+            cout << result[i].src << " -- " << result[i].dest << "    Weight: " << result[i].weight << endl;
+    }
+};
+
+int main() {
+    int V = 4;
+    Graph g(V);
+
+    g.addEdge(0, 1, 10);
+    g.addEdge(0, 2, 6);
+    g.addEdge(0, 3, 5);
+    g.addEdge(1, 3, 15);
+    g.addEdge(2, 3, 4);
+
+    g.kruskalMST();
+
+    return 0;
+}
